e3d067b035089000b1fb81ed0dd1167d9da48200
Paveloom <paveloom@mail.ru>
2019-05-06 15:45:04 +0300

    Тестирование алгоритма для учёта исключений: Исправлено выравнивание. Проверяю полезность исключений через включения проверок через операторы if.

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
-        comp := gfortran
-        opt  := -c -Wall -Wtabs
-        pattern:=*.f95
-        source :=$(wildcard $(pattern))
-        obj :=$(patsubst %.f95, %.o, $(source))
 
+      # Настройки компиляции программ
+           comp := gfortran
+            opt := -c -Wall -Wtabs
+        pattern := f95
+     allpattern := *.$(pattern)
+     anypattern := %.$(pattern)
+         source := $(wildcard $(allpattern))
+            mod := $(patsubst $(anypattern), %.mod, $(source))
+            obj := $(patsubst $(anypattern), %.o, $(source))
+
+      # Блок правил для компиляции объектных файлов
+      
         main : $(obj)
 	       $(comp) $^ -o $@
 
@@ -13,49 +20,100 @@
        %.mod : %.f95
 	       $(comp) $(opt) $<
 
-    main.o : subprograms.mod
+      # Блок правил-зависимостей (при необходимости)
+        main.o : testm.mod
 
-       result : main input
-	        time ./$<  < input > output
-       result-r : main input
-		rm output
-		make result
-		cat output
+      # Блок правил для инициализации make-файла для сборки программы
+      
+           input :
+	              touch input
+      
+          result : main input
+	              time ./$<  < input > output
+	        
+        result-r : main input
+		         rm output
+		         make result
+		         cat output
 
+      # Блок правил для очистки директории
         clean :
-	 rm -f *.o *.mod main
+	 rm -f $(obj) $(mod) main
 
         clean-all :
 	 rm -f *.o *.mod main *.eps *.dat result
 
+
+
+      # Блок правил для загрузки кода на Github
+
+        # Правила для проверки статуса репозитория
           git-s :
 		git status
 		git remote
 
-        ifeq (git,$(firstword $(MAKECMDGOALS)))
+        # Правило для загрузки на Github с указанием метки репозитория и сообщения коммита (см. Readme)
+
+        ifeq (git-r,$(firstword $(MAKECMDGOALS)))
         rep := $(wordlist 2,2,$(MAKECMDGOALS))
         m := $(wordlist 3,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
-        $(eval $(a):;#)
+        $(eval $(rep):;#)
+        $(eval $(m):;#)
+        endif
+
+          git-r :
+		git add -A
+		git commit -m "$(m)"
+		git push -u $(rep) master
+		
+	  # Правило для загрузки на Github с указанием сообщения коммита, но без указания метки репозитория (см. Readme)
+	  
+        ifeq (git,$(firstword $(MAKECMDGOALS)))
+        m := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+        $(eval $(m):;#)
         endif
 
           git :
 		git add -A
 		git commit -m "$(m)"
-		git push -u $(rep) master
+		git push -u origin master
 
-    git-clean :
+        # Правило для удаления репозитория в текущей директории
+          git-clean :
 		rm -rf .git
+
+       # Правило для подключения нового репозитория с указанием названия и метки
+       # и загрузки в него стартового make-файла (см. Readme)
+
+        ifeq (git-new-r,$(firstword $(MAKECMDGOALS)))
+        new_rep := $(wordlist 2,2,$(MAKECMDGOALS))
+        label := $(wordlist 3,3,$(MAKECMDGOALS))
+        $(eval $(new_rep):;#)
+        $(eval $(label):;# Успешно.)
+        endif
+
+         git-new-r :
+			make git-clean
+			git init
+			git remote add $(label) git@github.com:Paveloom/$(new_rep).git
+			git add Makefile
+			git commit -m "Стартовый make-файл."
+			git push -u $(label) master
+			
+       # Правило для подключения нового репозитория с указанием названия, но без указания метки,
+       # и загрузки в него стартового make-файла (см. Readme)
 
         ifeq (git-new,$(firstword $(MAKECMDGOALS)))
         new_rep := $(wordlist 2,2,$(MAKECMDGOALS))
-        label := $(wordlist 3,3,$(MAKECMDGOALS))
-        $(eval $(a):;#)
+        $(eval $(new_rep):;# Успешно.)
         endif
 
-      git-new :
-		make git-clean
-		git init
-		git remote add $(label) git@github.com:Paveloom/$(new_rep).git
-		git add Makefile
-		git commit -m "Стартовый make-файл."
-		git push -u $(label) master
+         git-new :
+			make git-clean
+			git init
+			git remote add origin git@github.com:Paveloom/$(new_rep).git
+			git add Makefile
+			git commit -m "Стартовый make-файл."
+			git push -u origin master
+			
+
--- Работа/main.f95
+++ Работа/main.f95
@@ -1,155 +1,160 @@
 program main
 implicit none
 
-real(8), allocatable, dimension(:,:) :: A ! Матрица исходных данных
-integer N                                 ! Размер выборки
+     real(8), allocatable, dimension(:,:) :: A ! Матрица исходных данных
+     integer N                                 ! Размер выборки
 
-! Коррелограмма:
+     ! Коррелограмма:
 
-real(8), allocatable, dimension(:) :: r ! Вектор коэффициентов корреляции
-real(8) x_mean                          ! Среднее значение x
-real(8) koef_mean                       ! Коэффициент 1/N
+     real(8), allocatable, dimension(:) :: r ! Вектор коэффициентов корреляции
+     real(8) x_mean                          ! Среднее значение x
+     real(8) koef_mean                       ! Коэффициент 1/N
 
-! Периодограмма:
+     ! Периодограмма:
 
-real(8), allocatable, dimension(:) :: I_p ! Вектор значений периодограммы
-real(8) p_step                            ! Шаг дискретизации частот
-integer p_num                             ! Число желаемых данных
-real(8) p_cur                             ! Текущее значение p
+     real(8), allocatable, dimension(:) :: I_p ! Вектор значений периодограммы
+     real(8) p_step                            ! Шаг дискретизации частот
+     integer p_num                             ! Число желаемых данных
+     real(8) p_cur                             ! Текущее значение p
 
-real(8) koef                 ! Коэффициент для тригонометрических функций
-real(8) cos_value, sin_value ! Вычисленные значения косинуса и синуса
-real(8) diff                 ! Разность (A(t,2) - x_mean)
+     real(8) koef                 ! Коэффициент для тригонометрических функций
+     real(8) cos_value, sin_value ! Вычисленные значения косинуса и синуса
+     real(8) diff                 ! Разность (A(t,2) - x_mean)
 
-! Общие переменные
+     ! Общие переменные
 
-real(8) s1, s2              ! Временные суммы для элементов выражений
-real(8) pi                  ! Число pi
-real(8) k_d, N_d, p_d, t_d  ! Перевод integer в double real
-integer k, t, i, p, p_num_d ! Вспомогательные переменные 
+     real(8) s1, s2              ! Временные суммы для элементов выражений
+     real(8) pi                  ! Число pi
+     real(8) k_d, N_d, p_d, t_d  ! Перевод integer в double real
+     integer k, t, i, p, p_num_d ! Вспомогательные переменные 
 
-! Указать размер выборки
-N = 5860
+     ! Указать размер выборки
+     N = 5860
 
-allocate(A(1:N,2)) ! Исходные данные
-allocate(r(1:N-1)) ! Для вычисления коррелограммы
+     allocate(A(1:N,2)) ! Исходные данные
+     allocate(r(1:N-1)) ! Для вычисления коррелограммы
 
-! Считывание исходных данных
+     ! Считывание исходных данных
 
         do i=1, N
                 read(*,*) A(i,1), A(i,2)
         enddo
 
-! Вычисление среднего значения выборки
+     ! Вычисление среднего значения выборки
 
-        x_mean = 0d0
+     x_mean = 0d0
 
-        N_d = N
-        koef_mean = 1d0/N_d
+     N_d = N
+     koef_mean = 1d0/N_d
 
-        do t=1, N
-           x_mean = x_mean + A(t,2)
-        enddo
-        x_mean = koef_mean * x_mean
+     do t = 1, N
+          
+          x_mean = x_mean + A(t,2)
+     
+     enddo
 
-! Вычисление коррелограммы
+     x_mean = koef_mean * x_mean
 
-        open(10, file="output1")
-        do k = 1, N - 1
+     ! Вычисление коррелограммы
+
+     open(10, file="output1")
+     do k = 1, N - 1
  
-                k_d = k
+          k_d = k
                 
-                s1 = 0d0
+          s1 = 0d0
   
-                do t = 1, N - k
-                        s1 = s1 + (A(t,2) - x_mean) * (A(t+k,2) - x_mean)
-                enddo
-
-                s2 = 0d0
-
-                do t = 1, N
-                        s2 = s2 + (A(t,2) - x_mean) * (A(t,2) - x_mean)
-                enddo
-
-                r(k) = s1/s2
-
-                write(10,'(e28.20, 1x, e28.20)') k_d, r(k)
-
-        enddo
-        close(10)
-
-! Определение pi
-pi = 4d0*datan(1d0)
-
-! Вычисление периодограммы
-
-p_num = 58600
-p_num_d = p_num
-p_step = N_d / p_num_d
-
-allocate(I_p(1:p_num))   ! Для вычисления периодограммы вне зависимости от c
-
-        open(11, file="output2")
-        do p = 1, p_num, 1
-
-                p_d = p
-                p_cur = 0d0 + p_d * p_step
-
-                s1 = 0d0
-                s2 = 0d0
-
-                do t = 1, N
-
-                        if ((t .eq. 2) .or. ((t .le. 8) .and. (t .ge. 4))) cycle
-                        if ((t .eq. 10) .or. (t .eq. 54) .or. (t .eq. 133)) cycle
-                        if ((t .ge. 1541) .and. (t .le. 1546)) cycle
-                        if ((t .ge. 2141) .and. (t .le. 2149)) cycle
-                        if ((t .ge. 2425) .and. (t .le. 2426)) cycle
-                        if ((t .ge. 2772) .and. (t .le. 2777)) cycle
-                        if ((t .ge. 2807) .and. (t .le. 2809)) cycle
-                        if ((t .ge. 2863) .and. (t .le. 2867)) cycle
-                        if ((t .ge. 2896) .and. (t .le. 2897)) cycle
-                        if (t .eq. 3004) cycle
-                        if ((t .ge. 3117) .and. (t .le. 3123)) cycle
-                        if ((t .ge. 3537) .and. (t .le. 3545)) cycle
-                        if ((t .ge. 3551) .and. (t .le. 3556)) cycle
-                        if ((t .ge. 3537) .and. (t .le. 3545)) cycle
-                        if ((t .ge. 3586) .and. (t .le. 3594)) cycle
-                        if ((t .ge. 3602) .and. (t .le. 3607)) cycle
-                        if ((t .ge. 3795) .and. (t .le. 3801)) cycle
-                        if ((t .ge. 3810) .and. (t .le. 3953)) cycle
-                        if ((t .ge. 3961) .and. (t .le. 4026)) cycle
-                        if ((t .ge. 3537) .and. (t .le. 3545)) cycle
-                        if ((t .eq. 5284) .or. (t .eq. 5287)) cycle
-                        if ((t .eq. 5613) .or. (t .eq. 5669)) cycle
-
-                        t_d = t
+          do t = 1, N - k
                         
-                        koef = 2d0 * pi * p_cur * t_d / N_d
-                        
-                        cos_value = dcos(koef)
-                        sin_value = dsin(koef)
-                        
-                                ! Проверка на ошибку округления sin_value и cos_value
-                                if (abs(cos_value) .le. 1e-3) cos_value = 0d0
-                                if (abs(sin_value) .le. 1e-3) sin_value = 0d0       
-                                                        
-                        diff = (A(t,2) - x_mean)
-                        
-                        s1 = s1 + diff * cos_value
-                        s2 = s2 + diff * sin_value
-                        
-                enddo
+               s1 = s1 + (A(t,2) - x_mean) * (A(t+k,2) - x_mean)
+          
+          enddo
 
-                I_p(p) = (s1 * s1 + s2 * s2)/(N_d * pi)
+          s2 = 0d0
 
-                write(11,'(e16.7, 1x, e16.7, 1x, e16.7)') N_d/p_cur, p_cur/N_d, I_p(p)
+          do t = 1, N
+               
+               s2 = s2 + (A(t,2) - x_mean) * (A(t,2) - x_mean)
+          
+          enddo
 
-        enddo
-        close(11)
+          r(k) = s1/s2
 
-deallocate(A)
-deallocate(r)
-deallocate(I_p)
+          write(10,'(e28.20, 1x, e28.20)') k_d, r(k)
+
+     enddo
+     close(10)
+
+     ! Определение pi
+     pi = 4d0*datan(1d0)
+
+     ! Вычисление периодограммы
+
+     p_num = 58600
+     p_num_d = p_num
+     p_step = N_d / p_num_d
+
+     allocate(I_p(1:p_num))   ! Для вычисления периодограммы вне зависимости от c
+
+     open(11, file="output2")
+     do p = 1, p_num, 1
+
+          p_d = p
+          p_cur = 0d0 + p_d * p_step
+
+          s1 = 0d0
+          s2 = 0d0
+
+          do t = 1, N
+
+                    if ((t .eq. 2) .or. ((t .le. 8) .and. (t .ge. 4))) cycle
+                    if ((t .eq. 10) .or. (t .eq. 54) .or. (t .eq. 133)) cycle
+                    if ((t .ge. 1541) .and. (t .le. 1546)) cycle
+                    if ((t .ge. 2141) .and. (t .le. 2149)) cycle
+                    if ((t .ge. 2425) .and. (t .le. 2426)) cycle
+                    if ((t .ge. 2772) .and. (t .le. 2777)) cycle
+                    if ((t .ge. 2807) .and. (t .le. 2809)) cycle
+                    if ((t .ge. 2863) .and. (t .le. 2867)) cycle
+                    if ((t .ge. 2896) .and. (t .le. 2897)) cycle
+                    if (t .eq. 3004) cycle
+                    if ((t .ge. 3117) .and. (t .le. 3123)) cycle
+                    if ((t .ge. 3537) .and. (t .le. 3545)) cycle
+                    if ((t .ge. 3551) .and. (t .le. 3556)) cycle
+                    if ((t .ge. 3537) .and. (t .le. 3545)) cycle
+                    if ((t .ge. 3586) .and. (t .le. 3594)) cycle
+                    if ((t .ge. 3602) .and. (t .le. 3607)) cycle
+                    if ((t .ge. 3795) .and. (t .le. 3801)) cycle
+                    if ((t .ge. 3810) .and. (t .le. 3953)) cycle
+                    if ((t .ge. 3961) .and. (t .le. 4026)) cycle
+                    if ((t .ge. 3537) .and. (t .le. 3545)) cycle
+                    if ((t .eq. 5284) .or. (t .eq. 5287)) cycle
+                    if ((t .eq. 5613) .or. (t .eq. 5669)) cycle
+     
+                    t_d = t
+                             
+                    koef = 2d0 * pi * p_cur * t_d / N_d
+                             
+                    cos_value = dcos(koef)
+                    sin_value = dsin(koef)
+                             
+                    ! Проверка на ошибку округления sin_value и cos_value
+                    if (abs(cos_value) .le. 1e-3) cos_value = 0d0
+                    if (abs(sin_value) .le. 1e-3) sin_value = 0d0       
+                                                             
+                    diff = (A(t,2) - x_mean)
+                             
+                    s1 = s1 + diff * cos_value
+                    s2 = s2 + diff * sin_value
+                             
+          enddo
+
+          I_p(p) = (s1 * s1 + s2 * s2)/(N_d * pi)
+
+          write(11,'(e16.7, 1x, e16.7, 1x, e16.7)') N_d/p_cur, p_cur/N_d, I_p(p)
+
+     enddo
+     close(11)
+
+     deallocate(A, r, I_p)
 
 end
